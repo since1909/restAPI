6041f81c4e0cd865e9dd1e7d1abeac0306f16846
since1909 <since1909@naver.com>
2021-10-04 22:53:48 +0900

    add controller

---
+++ src/main/java/com/restAPI/controller/UserInfoController.java
@@ -0,0 +1,36 @@
+package com.restAPI.controller;
+
+import com.restAPI.domain.model.UserInfo;
+import com.restAPI.dto.UserInfoDTO;
+import com.restAPI.service.UserInfoService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserInfoController {
+    final UserInfoService userInfoService;
+
+    public UserInfoController(UserInfoService userInfoService){
+        this.userInfoService = userInfoService;
+    }
+
+    @PostMapping("/userinfo")
+    public UserInfoDTO createUserInfo(@RequestBody UserInfoDTO userInfoDTO){
+        return userInfoService.createUserInfo(userInfoDTO);
+    }
+
+    @GetMapping("/userinfo/{name}")
+    public UserInfoDTO selectUserInfo(@PathVariable String name){
+        return userInfoService.selectUserInfo(name);
+    }
+
+    @PutMapping("/userinfo/{name}")
+    public UserInfoDTO updateUserInfo(@PathVariable String name, @RequestBody UserInfoDTO userInfoDTO){
+        return userInfoService.updateUserInfo(name, userInfoDTO);
+    }
+
+    @DeleteMapping
+    public String delteUserInfo(@PathVariable String name){
+        userInfoService.deleteUserInfo(name);
+        return "deleted " + name;
+    }
+}
